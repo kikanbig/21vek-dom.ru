3ead28f9722c04fb5432294a79171af33db30f30
Kiril Kaniushyk <kikanbig@gmail.com>
2025-12-10 13:10:17 +0300

    🐛 Исправлена проблема с URL (basename) и добавлено видео в проект

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/sets" element={<Sets />} />
